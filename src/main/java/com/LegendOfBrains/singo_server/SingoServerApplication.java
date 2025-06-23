ee53377a42db65d6ca1d496b2edadb6f0dc192ba
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-23 11:08:13 +0900

    project setting

---
+++ src/main/java/com/LegendOfBrains/singo_server/SingoServerApplication.java
@@ -0,0 +1,13 @@
+package com.LegendOfBrains.singo_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LegendOfBrains/singo_server/SingoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LegendOfBrains.singo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
