8884045fe159cc669afa3c03f3e138a4250e6700
서진교 <oygnijoes0209@gmail.com>
2025-06-25 15:17:43 +0900

    feat :: add DTO

---
+++ src/main/java/com/LegendOfBrains/singo_server/dto/EnrollDTO.java
@@ -0,0 +1,11 @@
+package com.LegendOfBrains.singo_server.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class EnrollDTO {
+    private String title;
+    private String content;
+}
